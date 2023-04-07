081bac9a637537995b22822d3d492d0a53800cc0
Temitopesam1 <temitopesam5@gmail.com>
2023-04-07 00:12:01 +0000

    doc: fix documentation in 3-glam_rock.sql file

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
--- This SQL script select band_name, and lifespan column which is difference
+-- This SQL script select band_name, and lifespan columns
 SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan
     FROM metal_bands
     WHERE FIND_IN_SET('Glam rock', IFNULL(NULL, style)) > 0
