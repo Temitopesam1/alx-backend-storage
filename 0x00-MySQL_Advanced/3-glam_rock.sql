ebf640bb165e6254dd7c048442c42cfcd4db12c3
Temi Sam <temitopesam5@gmail.com>
2023-04-08 20:46:36 +0100

    fix: fix bug in 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- This SQL script select band_name, and lifespan columns
 SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan
     FROM metal_bands
-    WHERE FIND_IN_SET('Glam rock', IFNULL(NULL, style)) > 0
+    WHERE style LIKE '%Glam rock%'
     ORDER BY lifespan DESC;
