714215e1032d6a1b87d4740f132cf253b2785eb9
Temi Sam <temitopesam5@gmail.com>
2023-04-12 01:16:19 +0100

    refactor: factor 12-log_stats.py in another way

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -4,16 +4,15 @@ from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    db_nginx = client.logs.nginx
+    client = MongoClient()
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-    count_logs = db_nginx.count_documents({})
+    count_logs = client.logs.nginx.count_documents({})
     print(f'{count_logs} logs')
 
     print('Methods:')
     for method in methods:
-        count_method = db_nginx.count_documents({'method': method})
+        count_method = client.logs.nginx.count_documents({'method': method})
         print(f'\tmethod {method}: {count_method}')
 
     check = db_nginx.count_documents(
