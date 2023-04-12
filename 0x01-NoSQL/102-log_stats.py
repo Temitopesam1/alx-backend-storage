47921a17a87ab0cb149a99fb218c4d0cc6d89d94
Temi Sam <temitopesam5@gmail.com>
2023-04-12 01:27:29 +0100

    refactor: Update 102-log_stats.py

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -5,24 +5,23 @@ from pymongo import MongoClient
 
 if __name__ == "__main__":
     client = MongoClient()
-    db_nginx = client.logs.nginx
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-    count_logs = db_nginx.count_documents({})
+    count_logs = client.logs.nginx.count_documents({})
     print(f'{count_logs} logs')
 
     print('Methods:')
     for method in methods:
-        count_method = db_nginx.count_documents({'method': method})
+        count_method = client.logs.nginx.count_documents({'method': method})
         print(f'\tmethod {method}: {count_method}')
 
-    check = db_nginx.count_documents(
+    check = client.logs.nginx.count_documents(
         {"method": "GET", "path": "/status"}
     )
 
     print(f'{check} status check')
     print("IPs:")
-    ips = db_nginx.aggregate([
+    ips = client.logs.nginx.aggregate([
         {"$group":
             {
                 "_id": "$ip",
